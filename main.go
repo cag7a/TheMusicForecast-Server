8958eaae6d5fbe3da85ee45a91ccadb29a54efab
cameron-justice <cameronjustice@protonmail.com>
2021-02-23 12:24:15 -0500

    basic net usage on load

--- main.go
+++ main.go
@@ -9,6 +9,11 @@ import (
 func main() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprintf(w, "The Music Forecast")
+		net := &Network{
+			Name: "Based Network",
+		}
+		InitializeNetwork(net, BinaryStep)
+
 		// http.ServeFile(w, r, 'client/index.html')
 	})
 
--- net.go
+++ net.go
@@ -1,5 +1,7 @@
 package main
 
+import "fmt"
+
 // ActivationFunction is an abstract header for any valid activation function
 type ActivationFunction func(float64) float64
 
@@ -11,12 +13,23 @@ type Network struct {
 // InitializeNetwork takes a pointer to a Network and prepares it for training.
 // Should always be the first action a Network takes
 func InitializeNetwork(net *Network, actType ActivationFunction) {
-	if !net.Name {
-
+	if net.Name == "" {
+		net.Name = "NewNetwork"
 	}
+
+	fmt.Println(net.Name)
 }
 
 // LoadWeights takes a pointer to a Network and the name of a weights file to be read in
 func LoadWeights(net *Network, fileName string) {
 
+}
+
+// BinaryStep activation function
+func BinaryStep(val float64) float64 {
+	if val > 0 {
+		return 1
+	}
+
+	return 0
 }
