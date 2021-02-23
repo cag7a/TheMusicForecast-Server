a339c9206db16d6e493bbeb95eec7ccfc6a1a523
cameron-justice <cameronjustice@protonmail.com>
2021-02-23 15:38:41 -0500

    Working neural net framework

--- main.go
+++ main.go
@@ -3,16 +3,32 @@ package main
 import (
 	"fmt"
 	"log"
+	"math/rand"
 	"net/http"
+	"time"
 )
 
 func main() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprintf(w, "The Music Forecast")
-		net := &Network{
-			Name: "Based Network",
+		net := New(4, 4, 4, 4, 0.005, Sigmoid, SigmoidPrime)
+
+		inData := [][]float64{{0.1, 0.4, 0.6, 0.5}, {0.1, 0.4, 0.6, 0.5}, {0.1, 0.4, 0.6, 0.5}, {0.1, 0.4, 0.6, 0.5}, {0.1, 0.4, 0.6, 0.5}}
+		targets := [][]float64{{0.1, 0.4, 0.6, 0.5}, {0.1, 0.4, 0.6, 0.5}, {0.1, 0.4, 0.6, 0.5}, {0.1, 0.4, 0.6, 0.5}, {0.1, 0.4, 0.6, 0.5}}
+
+		rand.Seed(time.Now().UTC().UnixNano())
+
+		for epochs := 0; epochs < 5000; epochs++ {
+			// Train the net on each example
+			for i, ix := range inData {
+				net.Train(ix, targets[i])
+			}
 		}
-		InitializeNetwork(net, BinaryStep)
+
+		pred := net.Predict(inData[0])
+
+		matrixPrint(pred)
+		fmt.Println(pred.Dims())
 
 		// http.ServeFile(w, r, 'client/index.html')
 	})
--- net.go
+++ net.go
@@ -1,35 +1,198 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math"
+	"os"
+
+	"gonum.org/v1/gonum/mat"
+	"gonum.org/v1/gonum/stat/distuv"
+)
 
 // ActivationFunction is an abstract header for any valid activation function
-type ActivationFunction func(float64) float64
+type ActivationFunction func(int, int, float64) float64
+
+// PrimeFunction is an abstract header for any valid backpropogation prime function
+type PrimeFunction func(mat.Matrix) mat.Matrix
 
 // Network is the global model of a Neural Network
 type Network struct {
-	Name string
+	Name          string
+	inputs        int
+	hiddens       int
+	outputs       int
+	hiddenWeights *mat.Dense
+	outputWeights *mat.Dense
+	learningRate  float64
+	act           ActivationFunction
+	prime         PrimeFunction
 }
 
-// InitializeNetwork takes a pointer to a Network and prepares it for training.
-// Should always be the first action a Network takes
-func InitializeNetwork(net *Network, actType ActivationFunction) {
-	if net.Name == "" {
-		net.Name = "NewNetwork"
+// New creates a new Network.
+func New(width int, depth int, inCount int, outCount int, learningRate float64, act ActivationFunction, prime PrimeFunction) (net *Network) {
+	net = &Network{
+		inputs:       inCount,
+		hiddens:      width * depth,
+		outputs:      outCount,
+		learningRate: learningRate,
+		act:          act,
+		prime:        prime,
 	}
 
-	fmt.Println(net.Name)
+	net.hiddenWeights = mat.NewDense(net.hiddens, net.inputs, randomArray(net.inputs*net.hiddens, float64(net.inputs)))
+	net.outputWeights = mat.NewDense(net.outputs, net.hiddens, randomArray(net.hiddens*net.outputs, float64(net.hiddens)))
+	return
 }
 
-// LoadWeights takes a pointer to a Network and the name of a weights file to be read in
-func LoadWeights(net *Network, fileName string) {
-
+func (net *Network) Predict(inputData []float64) mat.Matrix {
+	inputs := mat.NewDense(len(inputData), 1, inputData)
+	hiddenInputs := dot(net.hiddenWeights, inputs)
+	hiddenOutputs := apply(net.act, hiddenInputs)
+	finalInputs := dot(net.outputWeights, hiddenOutputs)
+	finalOutputs := apply(net.act, finalInputs)
+	return finalOutputs
 }
 
-// BinaryStep activation function
-func BinaryStep(val float64) float64 {
-	if val > 0 {
-		return 1
+func (net *Network) Train(inputData []float64, targetData []float64) {
+	// forward propagation
+	inputs := mat.NewDense(len(inputData), 1, inputData)
+	hiddenInputs := dot(net.hiddenWeights, inputs)
+	hiddenOutputs := apply(net.act, hiddenInputs)
+	finalInputs := dot(net.outputWeights, hiddenOutputs)
+	finalOutputs := apply(net.act, finalInputs)
+
+	// find errors
+	targets := mat.NewDense(len(targetData), 1, targetData)
+	outputErrors := subtract(targets, finalOutputs)
+	hiddenErrors := dot(net.outputWeights.T(), outputErrors)
+
+	// backpropagate
+	net.outputWeights = add(net.outputWeights,
+		scale(net.learningRate,
+			dot(multiply(outputErrors, net.prime(finalOutputs)),
+				hiddenOutputs.T()))).(*mat.Dense)
+
+	net.hiddenWeights = add(net.hiddenWeights,
+		scale(net.learningRate,
+			dot(multiply(hiddenErrors, net.prime(hiddenOutputs)),
+				inputs.T()))).(*mat.Dense)
+}
+
+func save(net Network) {
+	h, err := os.Create("data/hweights.model")
+	defer h.Close()
+	if err == nil {
+		net.hiddenWeights.MarshalBinaryTo(h)
+	}
+	o, err := os.Create("data/oweights.model")
+	defer o.Close()
+	if err == nil {
+		net.outputWeights.MarshalBinaryTo(o)
+	}
+}
+
+// load a neural network from file
+func load(net *Network) {
+	h, err := os.Open("data/hweights.model")
+	defer h.Close()
+	if err == nil {
+		net.hiddenWeights.Reset()
+		net.hiddenWeights.UnmarshalBinaryFrom(h)
+	}
+	o, err := os.Open("data/oweights.model")
+	defer o.Close()
+	if err == nil {
+		net.outputWeights.Reset()
+		net.outputWeights.UnmarshalBinaryFrom(o)
+	}
+	return
+}
+
+// Sigmoid activation function
+func Sigmoid(r, c int, z float64) float64 {
+	return 1.0 / (1 + math.Exp(-1*z))
+}
+
+// Sigmoid Prime activation function
+func SigmoidPrime(m mat.Matrix) mat.Matrix {
+	rows, _ := m.Dims()
+	o := make([]float64, rows)
+	for i := range o {
+		o[i] = 1
+	}
+	ones := mat.NewDense(rows, 1, o)
+	return multiply(m, subtract(ones, m)) // m * (1 - m)
+}
+
+func dot(m, n mat.Matrix) mat.Matrix {
+	r, _ := m.Dims()
+	_, c := n.Dims()
+	o := mat.NewDense(r, c, nil)
+	o.Product(m, n)
+	return o
+}
+
+func apply(fn func(i, j int, v float64) float64, m mat.Matrix) mat.Matrix {
+	r, c := m.Dims()
+	o := mat.NewDense(r, c, nil)
+	o.Apply(fn, m)
+	return o
+}
+
+func scale(s float64, m mat.Matrix) mat.Matrix {
+	r, c := m.Dims()
+	o := mat.NewDense(r, c, nil)
+	o.Scale(s, m)
+	return o
+}
+
+func multiply(m, n mat.Matrix) mat.Matrix {
+	r, c := m.Dims()
+	o := mat.NewDense(r, c, nil)
+	o.MulElem(m, n)
+	return o
+}
+
+func add(m, n mat.Matrix) mat.Matrix {
+	r, c := m.Dims()
+	o := mat.NewDense(r, c, nil)
+	o.Add(m, n)
+	return o
+}
+
+func subtract(m, n mat.Matrix) mat.Matrix {
+	r, c := m.Dims()
+	o := mat.NewDense(r, c, nil)
+	o.Sub(m, n)
+	return o
+}
+
+func addScalar(i float64, m mat.Matrix) mat.Matrix {
+	r, c := m.Dims()
+	a := make([]float64, r*c)
+	for x := 0; x < r*c; x++ {
+		a[x] = i
+	}
+	n := mat.NewDense(r, c, a)
+	return add(m, n)
+}
+
+func randomArray(size int, v float64) (data []float64) {
+	dist := distuv.Uniform{
+		Min: -1 / math.Sqrt(v),
+		Max: 1 / math.Sqrt(v),
 	}
 
-	return 0
+	data = make([]float64, size)
+	for i := 0; i < size; i++ {
+		data[i] = dist.Rand()
+	}
+
+	return
+}
+
+// pretty print a Gonum matrix
+func matrixPrint(X mat.Matrix) {
+	fa := mat.Formatted(X, mat.Prefix(""), mat.Squeeze())
+	fmt.Printf("%v\n", fa)
 }
