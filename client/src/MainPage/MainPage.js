f9ae057e5c91ec879158652aa1ce7d64376fba17
masturffect <ianseal10@gmail.com>
2021-03-13 14:02:44 -0600

    Added Header and background

---
+++ client/src/MainPage/MainPage.js
@@ -0,0 +1,16 @@
+import React, { Component } from "react";
+import "./MainPage.css";
+
+class MainPage extends Component {
+    render() {
+        return (
+            <div id = "MainPage" classname = "page-container">
+                <h1>The Music Forecast</h1>
+            
+            
+            
+            </div>
+        );
+    }
+}
+export default MainPage; 
