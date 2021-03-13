9fff55207d9fa1d5635c752ef1fcf03f2b6aa419
masturffect <ianseal10@gmail.com>
2021-03-13 15:04:08 -0600

    Added weather icons

--- client/src/MainPage/MainPage.js
+++ client/src/MainPage/MainPage.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import { WiDaySunny, WiCloud, WiRainWind, WiDayCloudy} from 'weather-icons-react';
 import "./MainPage.css";
 
 class MainPage extends Component {
@@ -6,9 +7,13 @@ class MainPage extends Component {
         return (
             <div id = "MainPage" classname = "page-container">
                 <h1>The Music Forecast</h1>
-            
-            
-            
+                <div className = "icon-container">
+                    <WiDaySunny size={200} color = '#000'/>
+                    <WiCloud size={200} color = '#000'/>
+                    <WiRainWind size={200} color = '#000'/>
+                    <WiDayCloudy size={200} color = '#000'/>
+                </div>
+
             </div>
         );
     }
